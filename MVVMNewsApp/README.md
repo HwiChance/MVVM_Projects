1794417554d421415638fe247d06f6647c57042c
HwiChance <gjtmzltoa@gmail.com>
2021-12-01 17:40:10 +0900

    Finish MVVMNewsApp

---
+++ MVVMNewsApp/README.md
@@ -0,0 +1,6 @@
+# News App with MVVM
+### Reference
+- [Philipp Lackner Youtube](https://www.youtube.com/playlist?list=PLQkwcJG4YTCRF8XiCRESq1IFFW8COlxYJ)
+
+### API
+- [News API](https://newsapi.org/)
---
+++ MVVMNewsApp/app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -0,0 +1,10 @@
+package com.androiddevs.mvvmnewsapp.util
+
+class Constants {
+    companion object {
+        const val API_KEY = "1b648bb4727e47e79c3e518ad2b423f8"
+        const val BASE_URL = "https://newsapi.org"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
+        const val QUERY_PAGE_SIZE = 20
+    }
+}
